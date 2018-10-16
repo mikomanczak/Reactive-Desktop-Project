5f278dd1a321bf4d657a80e4bae42a872a0e0214
Mikolaj Adam Manczak <mikomanczak@gmail.com>
2018-10-16 11:03:40 +0200

    Updates

---
+++ precache-manifest.af45ee198a87889cd28686358ec47c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Reactive-Desktop-Project/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "40609201b1aff364fbc0",
+    "url": "/Reactive-Desktop-Project/static/js/runtime~main.40609201.js"
+  },
+  {
+    "revision": "3519078cb56a4c7f5df9",
+    "url": "/Reactive-Desktop-Project/static/js/main.3519078c.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/Reactive-Desktop-Project/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "3519078cb56a4c7f5df9",
+    "url": "/Reactive-Desktop-Project/static/css/main.4c12191f.chunk.css"
+  },
+  {
+    "revision": "0505fd9cf94a202c2d180601a07722c9",
+    "url": "/Reactive-Desktop-Project/index.html"
+  }
+];
